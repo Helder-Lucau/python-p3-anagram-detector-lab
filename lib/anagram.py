cce157c669b3b2c143f6d09722d0523c0ececd22
Helder-Lucau <helderlucau@gmail.com>
2023-09-11 09:05:52 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, words):
+        self.words = words
+
+        return [e for e in self.words if sorted(self.word) == sorted(e)]
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
